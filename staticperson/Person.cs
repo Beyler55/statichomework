54f7dd6f22eabf461f900478e125b407aa7b7cc4
Baylar S. Sadigov <baylarss@code.local>
2022-05-31 18:06:00 +0400

    Static task

---
+++ staticperson/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace staticperson
+{
+    class Person
+    {
+        private static int _age;
+        public int Age
+        {
+            get
+            {
+                return ++_age;
+            }
+            set
+            {
+            }
+        }
+    }
+}
